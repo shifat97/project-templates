c93ec369dadd06880e7fa15caa74b2bac0a00767
shifat97 <shifatbinreza2@gmail.com>
2026-05-20 10:49:41 +0600

    feat: add table filter handler and enhance BasePage with select methods

---
+++ pytest-selenium-ai/utils/table_filter_handler.py
@@ -0,0 +1,16 @@
+def table_filter_handler(self, row_elements_locator):
+    rows = self.find_elements(row_elements_locator)
+    table_dict = []
+
+    for row in rows:
+        column = row.find_elements(*self.TABLE_COLUMN) 
+        row_data = {
+            "name": column[0].text,
+            "email": column[1].text,
+            "department": column[2].text,
+            "registration_id": column[3].text,
+            "age": column[4].text
+        }
+        table_dict.append(row_data)
+
+    return table_dict
